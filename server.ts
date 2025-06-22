702faeed87cd390f1e8d0f248c8405890206969e
David Almanza <dalmanza@rpa.com>
2025-06-22 08:34:14 -0600

    accessing the temple with the mighty credentials

---
+++ server.ts
@@ -0,0 +1,34 @@
+const username = "Not all those who wander";
+const password = "are lost";
+
+type TempleAccess = {
+    username: string;
+    password: string;
+}
+
+async function accessTemple (payload: TempleAccess) {
+    const url = `${process.env.API_URL}/v1/s1/e4/solution`;
+    console.log('url', url)
+    try {
+        const resp = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(payload),
+            headers: {
+                "API-KEY": process.env.API_KEY,
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (!resp.ok) {
+            throw new Error(`Error ${resp.statusText}`);
+        }
+        console.log("The gates were open and responded with: ",resp.statusText);
+    }
+    catch(error) {
+        console.log(error.message)
+    }
+}
+
+console.log("Accessing the Template with the Secrets...");
+
+accessTemple({username, password});
